e85afc69cb6a54dcf36f300f376cbf7590c64cce
Akula Divya <65438113+Divyaakula28@users.noreply.github.com>
2021-04-11 09:02:08 +0530

    Update settings.py

--- tvschedule/settings.py
+++ tvschedule/settings.py
@@ -12,10 +12,9 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
-import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
@@ -123,5 +122,4 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = '/static/'
-django_heroku.settings(locals())
 
