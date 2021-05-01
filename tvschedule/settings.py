65fc4f8fbe2df778f3019f32d21c5583dc1015e0
Akula Divya <65438113+Divyaakula28@users.noreply.github.com>
2021-05-01 16:00:39 +0530

    Update settings.py

--- tvschedule/settings.py
+++ tvschedule/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '!@p&3eg-d#t4zs(!pvz)*qv7y5657mljvjq27qsl-l#(vod)h^'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1','tvschedule.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1','.herokuapp.com']
 
 
 # Application definition
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'tv',
 ]
 
 MIDDLEWARE = [
