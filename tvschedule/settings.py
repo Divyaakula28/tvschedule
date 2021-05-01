4945d825855402d2e916271051c058df00345c81
Akula Divya <65438113+Divyaakula28@users.noreply.github.com>
2021-05-01 16:05:04 +0530

    Update settings.py

--- tvschedule/settings.py
+++ tvschedule/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'tv'
 ]
 
 MIDDLEWARE = [
@@ -115,10 +116,6 @@ USE_L10N = True
 
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.11/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATIC_URL = '/static/'
 
